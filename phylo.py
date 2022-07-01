3890062263da2a720547a2520aad55edfd7b70a7
Isaac Stead <isaac_stead@eva.mpg.de>
2022-07-01 21:12:21 +0200

    Whole tree likelihood, refactors

--- phylo.py
+++ phylo.py
@@ -1,13 +1,18 @@
-from collections import deque
+import collections
+import random
+import math
 
 import newick
-from numpy import array, ndarray
-from scipy.linalg import expm
+import numpy
+import scipy
 
 
 # Tree from Harmon (2022), 8.7 Appendix - Felsenstein's pruning algorithm
 TEST_TREE = newick.loads("((((A:1.0,B:1.0):0.5,C:1.5):1.0,(D:0.5,E:0.5)):2.0,F:2.5)")[0]
-TEST_Q = array([
+TEST_TIPS = {"A": 0, "B": 1, "C": 0, "D": 2, "E": 2, "F": 1}
+
+# The Mk model, which assumes that all state transitions are equally probable
+TEST_Q = numpy.array([
     [-2, 1, 1],
     [1, -2, 1],
     [1, 1, -2]
@@ -16,7 +21,7 @@ TEST_Q = array([
 
 def trait_state_probs(t, Q):
     """Calculate probability distribution of trait states at time t from rate matrix Q."""
-    return expm(Q * t)
+    return scipy.linalg.expm(Q * t)
 
 
 def branch_likelihood(state, tip_state_probs, branch_len, Q):
@@ -44,59 +49,59 @@ def tree_likelihood(Q, tree, tip_states):
     Assumes the tree is binary and in the Newick format where leaves are named
     and branch lengths are given.
     """
-    # Attach tip likelihoods to leaf nodes
+    # Attach tip likelihoods to leaf nodes. This is easy, the likelihood for state X at
+    # some tip is 1 if the tip has that state, otherwise it's 0.
     tips = tree.get_leaves()
-    n_states = max(tip_states.values())
+    n_states = max(tip_states.values()) + 1
+    likelihoods = {} # For storing computed likelihoods at each node. Keyed by id(node)
     for label, state_index in tip_states.items():
-        likelihoods = [1 if i == state_index else 0 for i in range(n_states+1)]
-        tree.get_node(label).likelihoods = likelihoods
-
-    # Traverse tree bottom up in breadth-first order, computing likelihood at each node
-    queue = deque(tips)
-    while queue:
-        print(queue)
-        node = queue.popleft()
-        if node.ancestor and node.ancestor not in queue:
-            queue.append(node.ancestor)
-        if node.descendants:
-            left, right = node.descendants
-            if hasattr(left, "likelihoods") and hasattr(right, "likelihoods"): # Fix
-                node.likelihoods = node_likelihood(
-                    Q, left.length, right.length, left.likelihoods, right.likelihoods
-                )
-    return tree.likelihoods
-            
-
-def tree_likelihood_dict(Q, tree, tip_states):
-    """Felsenstein's (1973) pruning algorithm. Calculate the root likelihood of a tree
-    Q: state transition rate matrix
-    tree: newick.Node instance containing root of tree
-    tip_states: dict of {leaf_name : state_index}
-    Assumes the tree is binary and in the Newick format where leaves are named
-    and branch lengths are given.
-    """
-    # Attach tip likelihoods to leaf nodes
-    tips = tree.get_leaves()
-    n_states = max(tip_states.values())
-    likelihoods = {} # For storing computed likelihoods at each node
-    for label, state_index in tip_states.items():
-        tip_likelihoods = [1 if i == state_index else 0 for i in range(n_states+1)]
+        tip_likelihoods = [1 if i == state_index else 0 for i in range(n_states)]
         tip = tree.get_node(label)
         likelihoods[id(tip)] = tip_likelihoods
 
     # Traverse tree bottom up in breadth-first order, computing likelihood at each node
-    queue = deque(tips)
+    queue = collections.deque(tips)
     while queue:
-        print(queue)
         node = queue.popleft()
         if node.ancestor and node.ancestor not in queue:
             queue.append(node.ancestor)
         if node.descendants:
             left, right = node.descendants
-            node_id, left_id, right_id = (id(node), id(left), id(right))
-            if left_id in likelihoods and right_id in likelihoods:
-                likelihoods[node_id] = node_likelihood(
-                    Q, left.length, right.length, likelihoods[left_id], likelihoods[right_id]
+            if id(left) in likelihoods and id(right) in likelihoods:
+                likelihoods[id(node)] = node_likelihood(
+                    Q, left.length, right.length, likelihoods[id(left)], likelihoods[id(right)]
                 )
-    return likelihoods[id(tree)]
+    # Now calculate the likelihood across the whole tree. For the moment I will assume
+    # the prior probability of each state is uniform.
+    prior = 1 / n_states
+    return sum([prior * l for l in likelihoods[id(tree)]])
+
+
+def unroot(tree):
+    """Convert a rooted tree to an unrooted tree"""
+    pass
+
+
+def nearest_neighbour_interchange(tree):
+    """The simplest algorithm for transforming a tree topology (Felsenstein 2004).
+    An interior branch of a subtree and the two branches connected to that branch at
+    each end are erased, so we're left with 4 subtrees. 4 subtrees can be joined into
+    a tree in 3 possible ways, one of which is the original subtree, so there are two
+    possibilities. 2(n - 3) 
+    """
+    # 1. Pick an internal node at random
+    node = random.choice([n for n in tree.walk() if n.descendants and n.ancestor])
+    # 2. It's got 4 connections: its ancestor, sibling, left child, right child
+    
+
+## Where next?
+# The likelihood function is different for each model of evolution. I reckon stick with
+# simple Mk and try doing metropolis hastings with the parameters being the tree topology
+# and the branch lengths, so look into the algorithms which manipulate the tree to suggest
+# new ones
+# Also thinking about picking up Julia, so could rewrite once I get a bit more written
+
+def test():
+    tl = tree_likelihood(TEST_Q, TEST_TREE, TEST_TIPS)
+    print(tl)
 
