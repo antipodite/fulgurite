a3b1dc9212eee2a55ab3db458fa5ffd36f17cc21
Isaac Stead <isaac_stead@eva.mpg.de>
2022-07-04 21:09:16 +0200

    Node code in own module

--- tests/test_tree.py
+++ tests/test_tree.py
@@ -1,7 +1,7 @@
 import pytest
 import anytree
 import copy
-import fulgurite.phylo as phylo
+from fulgurite.phylonode import PhyloNode, PhyloNodeError
 
 NEWICK = "((((A:1.0,B:1.0):0.5,C:1.5):1.0,(D:0.5,E:0.5)):2.0,F:2.5)"
 SWAPPED = "((((D:0.5,E:0.5),C:1.5):1.0,(A:1.0,B:1.0):0.5):2.0,F:2.5)"
@@ -9,12 +9,12 @@ SWAPPED = "((((D:0.5,E:0.5),C:1.5):1.0,(A:1.0,B:1.0):0.5):2.0,F:2.5)"
 
 ## Made it a function instead of var so pytest picks up any problems that might
 ## conceivably occur, maybe this is unnecessary?
-newtree = lambda: phylo.PhyloNode(label="A", children=[
-    phylo.PhyloNode(label="B", children=[
-        phylo.PhyloNode(label="C"),
-        phylo.PhyloNode(label="D")
+newtree = lambda: PhyloNode(label="A", children=[
+    PhyloNode(label="B", children=[
+        PhyloNode(label="C"),
+        PhyloNode(label="D")
     ]),
-    phylo.PhyloNode(label="E")
+    PhyloNode(label="E")
 ])
 
 
@@ -25,14 +25,14 @@ def test_tree_creation():
 
 def test_attach():
     tree = newtree()
-    node_f = phylo.PhyloNode(label="F")
+    node_f = PhyloNode(label="F")
     tree.attach(node_f)
     assert tree.is_binary
 
 
 def test_detach():
     tree = newtree()
-    with pytest.raises(phylo.PhyloNodeError):
+    with pytest.raises(PhyloNodeError):
         tree.detach(tree)
     for node in anytree.PreOrderIter(tree):
         if not node.is_root:
@@ -49,19 +49,19 @@ def test_properties():
 
 
 def test_from_string():
-    tree = phylo.PhyloNode.from_string(NEWICK)
+    tree = PhyloNode.from_string(NEWICK)
     assert tree.is_binary
 
 
 def test_eq():
-    tree1 = phylo.PhyloNode.from_string(NEWICK)
-    tree2 = phylo.PhyloNode.from_string(NEWICK)
+    tree1 = PhyloNode.from_string(NEWICK)
+    tree2 = PhyloNode.from_string(NEWICK)
     assert tree1.equal(tree2)
 
 
 def test_swap():
-    tree = phylo.PhyloNode.from_string(NEWICK)
-    swapped = phylo.PhyloNode.from_string(SWAPPED)
+    tree = PhyloNode.from_string(NEWICK)
+    swapped = PhyloNode.from_string(SWAPPED)
     a = tree.children[0].children[0].children[0]
     b = tree.children[0].children[1]
     print("Tree:\n", anytree.RenderTree(tree))
