01f48dc33b654f506117955ec50061493e0e49db
Isaac Stead <isaac_stead@eva.mpg.de>
2022-07-24 02:49:41 +0200

    Added test squamate data and code to load it

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Fulgurite
 
-Toy implementation of Bayesian phylogenetic analysis algorithms for learning purposes and experimentation with the method
+Toy implementation of Bayesian phylogenetic analysis algorithms for learning purposes and experimentation with the method.
+
+Contains test data from [Phylogenetic Comparative Methods](https://lukejharmon.github.io/pcm/chapters/) by Luke J. Harmon.
--- tests/test_tree.py
+++ tests/test_tree.py
@@ -1,32 +1,60 @@
 import anytree
 import logging
-import numpy
 import random
+import pathlib
+import csv
 
-from fulgurite.tree import PhyloNode, PhyloNodeError, PhyloTree
+from distutils import dir_util
+from fulgurite.tree import PhyloNode, PhyloTree
+from fulgurite.mcmc import sample
+
 
 LOGGER = logging.getLogger(__name__)
+DATADIR = pathlib.Path(__file__).parent / "data"
+
 # Tree from Harmon (2022), 8.7 Appendix - Felsenstein's pruning algorithm
 NEWICK = "((((A:1.0,B:1.0):0.5,C:1.5):1.0,(D:0.5,E:0.5)):2.0,F:2.5)"
 SWAPPED = "((((D:0.5,E:0.5),C:1.5):1.0,(A:1.0,B:1.0):0.5):2.0,F:2.5)"
 
-# The Mk model, which assumes that all state transitions are equally probable. This
-# rate matrix is for a character with 3 possible states
-TEST_Q = numpy.array([
-    [-2, 1, 1],
-    [1, -2, 1],
-    [1, 1, -2]
-])
 # Tip states for testing likelihood function
 TEST_TIPS = {"A": 0, "B": 1, "C": 0, "D": 2, "E": 2, "F": 1}
 
 
+def load_squamate_data():
+    """Load the squamate tree from Harmon (2022).
+    Attach tip states for the "has limbs" character, this needs to be
+    calculated from the forelimb and hindlimb lengths.
+    """
+    with open(DATADIR / "brandley_table.csv") as f:
+        reader = csv.DictReader(f)
+        rows = [r for r in reader]
+    # Extract the "has limbs" character from the data. Need to count species
+    # with "FLL" and "HLL" = 0  as limbless, others as limbed
+    states = {}
+    for row in rows:
+        name = row["Species"].replace(" ", "_")
+        is_limbless = int(row["HLL"] == row["FLL"] == "0")
+        states[name] = is_limbless
+    # Create the squamate tree with the generated tip states for the "has
+    # limbs" character
+    with open(DATADIR / "squamate.phy") as f:
+        tree = PhyloTree.from_string(f.read(), states=states)
+    return tree
+
+
+def test_squamate_tree():
+    """Confirm squamate data for "is limbless" character loaded correctly"""
+    tree = load_squamate_data()
+    LOGGER.debug(str(tree))
+    assert len([x for x in tree.leaves if x.state == 1]) == 51
+    assert tree.root.is_binary
+    
 def test_tree_creation():
     tree = PhyloNode.from_string(NEWICK, states=TEST_TIPS)
     LOGGER.debug("Tree with tips:\n" + str(anytree.RenderTree(tree)))
     assert tree.is_binary
     assert all([n.state != None and n.likelihoods for n in tree.leaves])
-
+    
 
 def test_attach():
     tree = PhyloNode.from_string(NEWICK)
@@ -73,3 +101,6 @@ def test_phylotree_creation():
     LOGGER.debug(str(tree))
 
 
+def test_mcmc():
+    tree = PhyloTree.from_string(NEWICK, TEST_TIPS)
+    
